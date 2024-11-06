eda74858645394e3287fac8fafc17b1a4e5c3532
ArthurKValladares <arthurkaukal@gmail.com>
2024-11-06 23:17:01 +0100

    starting on some token stuff

--- src/main.cpp
+++ src/main.cpp
@@ -15,62 +15,84 @@ long get_file_size(FILE* file) {
     return sz;
 }
 
-int run(char* program) {
-    debug_log(program);
+struct KauCompiler {
+    bool m_had_error = false;
 
-    return 0;
-}
+    void error(int line, const char* message) {
+        report(line, "", message);
+    }
+    void report(int line, const char* where, const char* message) {
+        fprintf(stderr, "[Line %d] Error %s: %s\n", line, where, message);
+        m_had_error = true;
+    }
 
-int run_prompt() {
-    constexpr long max_line_size = 1024;
-    char line_char_buffer[max_line_size];
+    int run(char* program) {
+        debug_log(program);
 
-    while (true) {
-        fprintf(stdout, "> ");
-        fflush(stdout);
-        std::cin.getline(line_char_buffer, max_line_size);
-        if (strlen(line_char_buffer) == 0) {
-            break;
+        return 0;
+    }
+    int run_prompt() {
+        constexpr long max_line_size = 1024;
+        char line_char_buffer[max_line_size];
+
+        while (true) {
+            fprintf(stdout, "> ");
+
+            std::cin.getline(line_char_buffer, max_line_size);
+            if (strlen(line_char_buffer) == 0) {
+                break;
+            }
+
+            run(line_char_buffer);
+
+            if (m_had_error) {
+                // TODO: Do I do anything here?
+            }
+            m_had_error = false;
         }
-        run(line_char_buffer);
+
+        return 0;
     }
+    int run_file(char* file_path) {
+        FILE* script_file;
+        script_file = fopen(file_path, "r");
+        if (script_file == NULL) {
+            fprintf(stderr, "Failed to open kau script at: %s\n", file_path);
+            return -1;
+        }
+        const long file_size_bytes = get_file_size(script_file);
 
-    return 0;
-}
+        char* byte_buffer;
+        byte_buffer = (char*) malloc(file_size_bytes + 1);
+        const int end = fread(byte_buffer, 1, file_size_bytes, script_file);
+        byte_buffer[end] = '\0';
 
-int run_file(char* file_path) {
-    FILE* script_file;
-    script_file = fopen(file_path, "r");
-    if (script_file == NULL) {
-        fprintf(stderr, "Failed to open kau script at: %s\n", file_path);
-        return -1;
+        fclose(script_file);
+
+        run(byte_buffer);
+
+        if (m_had_error) {
+            return -1;
+        }
+
+        return 0;
     }
-    const long file_size_bytes = get_file_size(script_file);
-
-    char* byte_buffer;
-    byte_buffer = (char*) malloc(file_size_bytes + 1);
-    const int end = fread(byte_buffer, 1, file_size_bytes, script_file);
-    byte_buffer[end] = '\0';
-
-    fclose(script_file);
-
-    run(byte_buffer);
-
-    return 0;
-}
+};
 
 int main(int argc, char **argv) {
+    KauCompiler kau;
+
     switch (argc) {
         case 1: {
             debug_log("PROMPT");
 
-            run_prompt();
+            kau.run_prompt();
             break;
         }
         case 2: {
             debug_log("FILE");
 
-            run_file(argv[1]);
+            kau.run_file(argv[1]);
             break;
         }
         default: {
---
+++ src/tokens.h
@@ -0,0 +1,29 @@
+#include <string>
+
+enum class TokenType {
+    // Single-Character tokens
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+    // One or two character tokens
+    BANG, BANG_EQUAL,
+    EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL,
+    LESS, LESS_EQUAL,
+
+    // Literals
+    IDENTIFIER, STRING, NUMBER,
+
+    // Keywords
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+    _EOF
+};
+
+class Token {
+    TokenType m_type;
+    std::string m_lexeme;
+    std::string m_literal;
+    int m_line;
+};
