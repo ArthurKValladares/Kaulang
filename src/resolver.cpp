0b8cf24604e8ad26a4cfd4b2fdbd2db43cacb822
ArthurKValladares <arthurkaukal@gmail.com>
2025-01-20 21:42:00 -0500

    some more resolver work

--- src/resolver.cpp
+++ src/resolver.cpp
@@ -2,27 +2,21 @@
 
 void Resolver::resolve(Stmt* stmts, u64 stmts_len) {
     for (u64 i = 0; i < stmts_len; ++i) {
-        resolve_stmt(stmts[i]);
+        resolve_stmt(&stmts[i]);
     }
 }
 
-void Resolver::resolve_stmt(Stmt& stmt) {
-    switch (stmt.ty) {
+void Resolver::resolve_stmt(Stmt* stmt) {
+    switch (stmt->ty) {
         case Stmt::Type::EXPR: {
             break;
         }
         case Stmt::Type::VAR_DECL: {
-            declare(stmt.s_var_decl.name);
-            if (stmt.s_var_decl.expr != nullptr) {
-                resolve_expr(stmt.s_var_decl.expr);
-            }
-            define(stmt.s_var_decl.name);
+            visit_var_stmt(stmt);
             break;
         }
         case Stmt::Type::BLOCK: {
-            begin_scope();
-            resolve(stmt.s_block.stmts, stmt.s_block.size);
-            end_scope();
+            visit_block_stmt(stmt);
             break;
         }
         case Stmt::Type::IF: {
@@ -38,6 +32,7 @@ void Resolver::resolve_stmt(Stmt& stmt) {
             break;
         }
         case Stmt::Type::FN_DECLARATION: {
+            visit_fn_stmt(stmt);
             break;
         }
         case Stmt::Type::RETURN: {
@@ -54,6 +49,101 @@ void Resolver::resolve_stmt(Stmt& stmt) {
 }
 
 void Resolver::resolve_expr(Expr* expr) {
+    switch (expr->ty) {
+        case Expr::Type::LITERAL: {
+            // TODO: Don't love that this is how I need to detect a variable, make it better
+            if (expr->expr.literal->val->m_type == TokenType::IDENTIFIER) {
+                visit_variable_expr(expr);
+            }
+            break;
+        }
+        case Expr::Type::UNARY: {
+            break;
+        }
+        case Expr::Type::BINARY: {
+            break;
+        }
+        case Expr::Type::GROUPING: {
+            break;
+        }
+        case Expr::Type::TERNARY: {
+            break;
+        }
+        case Expr::Type::ASSIGNMENT: {
+            visit_assign_expr(expr);
+            break;
+        }
+        case Expr::Type::AND: {
+            break;
+        }
+        case Expr::Type::OR: {
+            break;
+        }
+        case Expr::Type::FN_CALL: {
+            break;
+        }
+        case Expr::Type::ERR: {
+            assert(false);
+            break;
+        }
+    }   
+}
+
+void Resolver::visit_block_stmt(Stmt* stmt) {
+    begin_scope();
+    resolve(stmt->s_block.stmts, stmt->s_block.size);
+    end_scope();
+}
+
+void Resolver::visit_var_stmt(Stmt* stmt) {
+    declare(stmt->s_var_decl.name);
+    if (stmt->s_var_decl.expr != nullptr) {
+        resolve_expr(stmt->s_var_decl.expr);
+    }
+    define(stmt->s_var_decl.name);
+}
+
+void Resolver::visit_fn_stmt(Stmt* stmt) {
+    declare(stmt->fn_declaration.name);
+    define(stmt->fn_declaration.name);
+
+    resolve_fn(stmt);
+}
+
+void Resolver::visit_variable_expr(Expr* expr) {
+    const Token* token = expr->expr.literal->val;
+    if (!scopes.empty() &&
+        scopes.back().contains(token->m_lexeme) &&
+        scopes.back().at(token->m_lexeme) == false) {
+        std::println(stderr, "Can't read local varaible in its own initializer");
+        exit(-1);
+    }
+    resolve_local(expr, token);
+}
+
+void Resolver::visit_assign_expr(Expr* expr) {
+    resolve_expr(expr->expr.assignment->right);
+    resolve_local(expr, expr->expr.literal->val);
+}
+
+void Resolver::resolve_local(Expr* expr, const Token* token) {
+    for (u64 i = scopes.size() - 1; i >= 0; --i) {
+        if (scopes[i].contains(token->m_lexeme)) {
+            mark_resolved(expr, scopes.size() - 1 - i);
+            return;
+        }
+    }
+}
+
+void Resolver::resolve_fn(Stmt* stmt) {
+    begin_scope();
+    for (u64 i = 0; i < stmt->fn_declaration.params_size; ++i) {
+        Token* param = stmt->fn_declaration.params[i];
+        declare(param);
+        define(param);
+    }
+    resolve_stmt(stmt->fn_declaration.body);
+    end_scope();
 }
 
 void Resolver::declare(Token* name) {
@@ -80,4 +170,8 @@ void Resolver::begin_scope() {
 
 void Resolver::end_scope() {
     scopes.pop_back();
+}
+
+void Resolver::mark_resolved(Expr* expr, int depth) {
+    // TODO
 }
--- src/resolver.h
+++ src/resolver.h
@@ -11,14 +11,25 @@ using ScopeMap = std::unordered_map<String, bool, StringHasher>;
 struct Resolver {
     void resolve(Stmt* stmts, u64 stmts_len);
 private:
-    void resolve_stmt(Stmt& stmt);
-    void resolve_expr(Expr* expr);
-    
+    void visit_block_stmt(Stmt* stmt);
+    void visit_var_stmt(Stmt* stmt);
+    void visit_fn_stmt(Stmt* stmt);
+    void visit_variable_expr(Expr* expr);
+    void visit_assign_expr(Expr* expr);
+
     void declare(Token* name);
     void define(Token* name);
 
     void begin_scope();
     void end_scope();
+
+    void resolve_stmt(Stmt* stmt);
+    void resolve_expr(Expr* expr);
+    void resolve_fn(Stmt* stmt);
+
+    void resolve_local(Expr* expr, const Token* token);
+
+    void mark_resolved(Expr* expr, int depth);
 
     // TOOD: using std structures for now
     std::vector<ScopeMap> scopes;
